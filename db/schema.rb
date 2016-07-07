95e2cb6107d13f25b27f0388943b45c12770b288
Joel Freedman <jfreedman@pivotal.io>
2016-07-07 13:52:03 +1000

    Updated DB to use PSQL
    [#125383551]
    
    Signed-off-by: Gabriel Bieules <gabriel.bieules@team.telstra.com>

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20160707010716) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "tasks", force: :cascade do |t|
     t.string   "description"
     t.datetime "created_at",  null: false
